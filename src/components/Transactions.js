b7a8a3ef0233cb7b64676665f2eb2fe9867774c5
tanyagoyal15 <tanyagoyal15@gmail.com>
2019-12-25 00:21:30 +0530

    modified css

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "../App.css"
 
 export class Transactions extends Component {
     constructor(props) {
@@ -8,7 +9,7 @@ export class Transactions extends Component {
             filteredTransactions: [],
             inputValue: '',
             currentPage: 1,
-            transactionsPerPage: 10
+            transactionsPerPage: 10,
         }
     }
 
@@ -18,26 +19,27 @@ export class Transactions extends Component {
                 return res.json();
             })
             .then(res => this.setState({ transactions: res , filteredTransactions: res }))
-            .then(console.log(this.state.filteredTransactions))
     }
 
     handleInput = (e) => {
         this.setState({ inputValue: e.target.value });
-        this.listItems();
+        this.listTransactions();
     }
  
     handleClick = (event) => {
+        event.preventDefault()
         this.setState({
-            currentPage: Number(event.target.id)
+            currentPage: Number(event.target.id),
         });
     }
 
-    listItems() {
+    listTransactions() {
         this.setState(state => {
             if (state.inputValue !== '') {
                 return {
                     filteredTransactions: state.transactions.filter(b =>
-                        b["Transaction Details"].toLowerCase().includes(this.state.inputValue.toLowerCase())
+                        b["Transaction Details"].toLowerCase().includes(`${this.state.inputValue.toLowerCase()}`) || 
+                        b["Date"].toLowerCase().includes(this.state.inputValue.toLowerCase()) 
                     )
                 }
             }
@@ -46,7 +48,7 @@ export class Transactions extends Component {
         })
     }
     render() {
-        const { transactions, filteredTransactions , currentPage, transactionsPerPage } = this.state;
+        const {transactions , filteredTransactions , currentPage, transactionsPerPage } = this.state;
 
         const indexOfLastTransaction = currentPage * transactionsPerPage;
         const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
@@ -58,15 +60,15 @@ export class Transactions extends Component {
                 <li key={idx}>{transaction["Date"]}</li>
                 <li key={idx}>{transaction["Transaction Details"]}</li>
                 <li key={idx}>{transaction["Value Date"]}</li>
-                <li key={idx}>{transaction["Withdrawal AMT"]}</li>
-                <li key={idx}>{transaction["Deposit AMT"]}</li>
+                <li key={idx}>{transaction["Withdrawal AMT"] === '' ? transaction["Withdrawal AMT"] = '-' : transaction["Withdrawal AMT"] = `${transaction["Withdrawal AMT"]}`}</li>
+                <li key={idx}>{transaction["Deposit AMT"] === '' ? transaction["Deposit AMT"] = '-' : transaction["Deposit AMT"] = `${transaction["Deposit AMT"]}`}</li>
                 <li key={idx}>{transaction["Balance AMT"]}</li>
             </ul>
         })
 
 
         const pageNumbers = [];
-        for (let i = 1; i <= Math.ceil(transactions.length / transactionsPerPage); i++) {
+        for (let i = 1; i <= Math.ceil(filteredTransactions.length / transactionsPerPage); i++) {
             pageNumbers.push(i);
         }
         const renderPageNumbers = pageNumbers.map(number => {
@@ -75,8 +77,7 @@ export class Transactions extends Component {
                     key={number}
                     id={number}
                     onClick={this.handleClick}
-                    style={{borderRight : '1px solid #ccc' ,padding : '5px' , cursor : 'pointer'}}
-                >
+                    className="pagenumbers">               
                     {number}
                 </li>
             );
@@ -85,7 +86,7 @@ export class Transactions extends Component {
         return (
             <React.Fragment>
                 <div className="container">
-                    <input type="text" placeholder="Search for transaction details..." className="searchbar" onChange={(e) => this.handleInput(e)} />
+                    <input type="text" placeholder="Search for transaction details by Date and recipient..." className="searchbar" onChange={(e) => this.handleInput(e)} />
                 </div>
                 <div className="transactions-container">
                     <ul className="transactionshead">
@@ -99,7 +100,8 @@ export class Transactions extends Component {
                     </ul>
                     <div style={{paddingBottom: '20px'}}>
                         {renderTransactions}
-                        <ul id="page-numbers">
+                        <ul id="pagination">
+                            <span style={{padding : '2px' , marginRight : '5%'}}>Page {this.state.currentPage}/{pageNumbers.length}</span>
                             {renderPageNumbers}
                         </ul>
                     </div>
