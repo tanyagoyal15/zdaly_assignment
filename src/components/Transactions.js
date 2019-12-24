f0af488f440a085d1136370f80dcb7bcf7844f17
tanyagoyal15 <tanyagoyal15@gmail.com>
2019-12-24 22:22:15 +0530

    added search functionality

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -5,6 +5,8 @@ export class Transactions extends Component {
         super(props);
         this.state = {
             transactions : [],
+            filteredTransactions: [],
+            inputValue: '',
             currentPage: 1,
             transactionsPerPage: 10
         }
@@ -15,21 +17,40 @@ export class Transactions extends Component {
             .then(res => {
                 return res.json();
             })
-            .then(res => this.setState({ transactions: res }))
+            .then(res => this.setState({ transactions: res , filteredTransactions: res }))
+            .then(console.log(this.state.filteredTransactions))
     }
 
+    handleInput = (e) => {
+        this.setState({ inputValue: e.target.value });
+        this.listItems();
+    }
+ 
     handleClick = (event) => {
         this.setState({
             currentPage: Number(event.target.id)
         });
     }
 
+    listItems() {
+        this.setState(state => {
+            if (state.inputValue !== '') {
+                return {
+                    filteredTransactions: state.transactions.filter(b =>
+                        b["Transaction Details"].toLowerCase().includes(this.state.inputValue.toLowerCase())
+                    )
+                }
+            }
+
+            return { filteredTransactions: state.transactions }
+        })
+    }
     render() {
-        const { transactions, currentPage, transactionsPerPage } = this.state;
+        const { transactions, filteredTransactions , currentPage, transactionsPerPage } = this.state;
 
         const indexOfLastTransaction = currentPage * transactionsPerPage;
         const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
-        const currentTransactions = transactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
+        const currentTransactions = filteredTransactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
 
         const renderTransactions = currentTransactions.map((transaction , idx) => {
             return <ul className="transactions">
@@ -54,7 +75,7 @@ export class Transactions extends Component {
                     key={number}
                     id={number}
                     onClick={this.handleClick}
-                    style={{border : '1px solid #ccc' , padding : '5px' , cursor : 'pointer'}}
+                    style={{borderRight : '1px solid #ccc' ,padding : '5px' , cursor : 'pointer'}}
                 >
                     {number}
                 </li>
@@ -62,32 +83,28 @@ export class Transactions extends Component {
         });
 
         return (
-            <div className="transactions-container">
-                <ul className="transactionshead">
-                    <li>S.no</li>
-                    <li>Date</li>
-                    <li>Transaction Details</li>
-                    <li>Value Date</li>
-                    <li>Withdrawal AMT</li>
-                    <li>Deposit AMT</li>
-                    <li>Balance AMT</li>
-                </ul>
-                <div>
-                    {renderTransactions}
-                    {
-                        renderTransactions.forEach(function (el) {
-                            Object.keys(el).forEach(function (property) {
-                                if (el[property] === '') {
-                                    el[property] = '-';
-                                }
-                            });
-                        })
-                    }
-                    <ul id="page-numbers">
-                        {renderPageNumbers}
-                    </ul>
+            <React.Fragment>
+                <div className="container">
+                    <input type="text" placeholder="Search for transaction details..." className="searchbar" onChange={(e) => this.handleInput(e)} />
                 </div>
-            </div>
+                <div className="transactions-container">
+                    <ul className="transactionshead">
+                        <li>S.no</li>
+                        <li>Date</li>
+                        <li>Transaction Details</li>
+                        <li>Value Date</li>
+                        <li>Withdrawal AMT</li>
+                        <li>Deposit AMT</li>
+                        <li>Balance AMT</li>
+                    </ul>
+                    <div style={{paddingBottom: '20px'}}>
+                        {renderTransactions}
+                        <ul id="page-numbers">
+                            {renderPageNumbers}
+                        </ul>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
